3c41a59c95ea26caea6a01daa10b853e624528fb
Ximing Chen <ximing.chen@verizonmedia.com>
2019-06-18 16:16:49 -0700

    use goimports to sort the import package

--- request_handler.go
+++ request_handler.go
@@ -29,22 +29,18 @@ type Page struct {
 	Description string
 }
 
-
 func readLines(path string) ([]string, error) {
 	file, err := os.Open(path)
 
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	defer file.Close()
-
 	var lines []string
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		lines = append(lines, scanner.Text())
 	}
-
 	return lines, scanner.Err()
 }
 
